a7b8e07bf091baada26673176a6553f7f9d4cae7
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-10-01 18:09:00 -0300

    Máscara de CPF

---
+++ ex2625.sql
@@ -0,0 +1,5 @@
+select FORMAT(CONVERT(bigint, np.cpf), '###\.###\.###-##') as cpf
+	from natural_person as np; --SQL Server
+
+select concat_ws('-', concat_ws('.', substring(np.cpf from 1 for 3), substring(np.cpf from 4 for 3), substring(np.cpf from 7 for 3)), substring(np.cpf from 10 for 2)) as cpf
+	from natural_person as np; --PostgreSQL
